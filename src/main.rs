5cff94d82ccdaff2b93e275e9631948b8531d90e
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-10-19 23:26:13 +0900

    💥 Update: rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,80 @@
-// [Rust入門書 : 第2章] https://doc.rust-jp.rs/book-ja/ch02-00-guessing-game-tutorial.html
+// Rust入門書第2章参照: https://doc.rust-jp.rs/book-ja/ch02-00-guessing-game-tutorial.html
 
-// std(標準ライブラリ)であるio(入出力ライブラリ)
-use std::{ io, cmp::Ordering }; // io(入出力)モジュール, 比較モジュール
-use rand::Rng; //乱数モジュール
-
-struct Guess { value: i32 }
-impl Guess {
-    pub fn new(value:i32) -> Guess {
-        if 1 > value || 100 < value { // if 1未満 or 100より大きい {}
-            panic!("範囲外ゐ値 {} を取得, 1~100まてゐ整数を入力(しなさい)", value)
-        }
-        Guess { value } // 初期化省略記法でGuessインスタンスを返す
-    }
-    // インスタンス.value では取り出せない(非公開)ので
-    // 専用のメソッドを実装
-    pub fn get_value(&self) -> i32 { self.value }
-}
+// std(標準ライブラリ)
+use std::{ io::stdin, cmp::Ordering }; // io(入出力)モジュール, 比較モジュール
+use rand::Rng; // RNG
 
 fn main() {
-    // rand::thread_rng関数は gen_rangeメソッド(乱数生成器)を返す
-    // gen_rangeメソッドは 第1引数-1 から 第2引数-1 までの乱数を生成する(OSからシード値を得て生成。)
+    // これが当てるべき数値
+    // rand::thread_rng関数は乱数生成器を初期化する
+    // gen_rangeメソッドは 第1引数-1 ~ 第2引数-1 までの範囲で乱数を生成する (OSからシード値を得て生成。)
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    /*println!("The secret number is: {}", secret_number);*/
-
-    println!("出力されゑ数價を的中(しなさい) [ 0~100 ]"); // 数を当ててごらん
+    println!("出力されゑ整数値を的中(しなさい) [ 0~100 ]");
 
     loop {
-        println!("早く入力レて！"); // ほら、予想を入力してね
+        println!("早く入力レて！");
 
-        // ユーザ入力を取得する変数
-        // Rust では変数はデフォルトで不変
-        // "let mut" とすることで可変にできる
-        // new関数がStringの関連関数*になっていることを :: が示す
-        // *関連関数とは型(ここではstr)に対して実装された関数。静的(static)メソッドと呼ばれることもある
-        // new関数とは新しく空の文字列を生成する関数
-        // つまり空の文字列を可変変数guess に代入しているということになる
+        // ユーザーの入力を持たせる変数
         let mut guess = String::new();
 
-        // ioモジュールの stdin関数
-        // 事前に　"use std::io" としない場合は関数を std::io::stdin() と記載する
-        // stdin関数は std::io::Stdin OBJを返し、それはターミナルの標準入力への標準入力ハンドル(識別子)となる(?)
-        // .read_line() は標準入力ハンドルの read_lineメソッドであり、入力された物を引数にある可変変数に投げる
-        // & はその引数が参照であることを表す。Rust では楽に安全に参照を扱える
-        // なお、参照もデフォルトで不変なので mut を付けて可変にする
-        io::stdin().read_line(&mut guess)
-            // この .expectメソッドは .read_lineメソッドに同じ行で連結出来るがこの様に改行することも可能
-            // 前の .read_lineメソッド は io::Result(ioモジュールでのResult型)を返す
-            // Result型はエラー情報をコード化する為のもので、列挙子(種類?)は Ok と Err
-            // Ok は成功時のもので生成された値を持つ
-            // Err は失敗時のもので失敗した過程や理由などをの情報を持つ
-            // そして「ioモジュールのResult型」(io::Result)にはexpectメソッドがある
-            // Result が Err値の場合 expectメソッドはプログラムを中止して引数にある物を表示する
-            // Result が Ok値 の場合 expectメソッドは Ok値が保持する値を返す
-            // なお、Result値を無視するとコンパイラに怒られる模様
-            .expect("行ゐ読込に失敗レた！\n大變た！\n刂トラ亻."); // 行の読み込みに失敗しました
+        // stdin関数の返す std::io::Stdin型は ターミナルの標準入力(stdin)へのハンドル
+        // read_lineメソッド ユーザーに入力された文字列を 引数として指定された可変変数に格納する
+        stdin().read_line(&mut guess)
+            // 読み取りに失敗した場合はパニックさせる
+            .expect("行ゐ読込に失敗レた！\n大變た！\n刂トラ亻."); 
 
-        // そして、guess (str)と secret_number (int)は型が違うので guess を以下のように...
-        // guess という変数はすでに存在するが元の guess を基にして u32型のguess　に入れ替える
-        // guess に trimメソッドが呼び出されていて、こいつが両端の空白や "\n" を全て消してくれる。
-        // 何故消すのかというと u32型(32bit非負数字) には数字しか含めない故
-        // ユーザが read_line の処理を終えるのに Enter を押す。すると改行文字(\n)が文字列に入ってしまう。
-        // perseメソッドは天才的頭脳で数値型を示唆してくれる
-        // ":" でコンパイラに変え先変数の型を注釈する合図をして u32(上限が低い自然数型)を注釈する
-        // これで secret_number変数と比較ができる
-        // ここまで u32型にする前提で話したが更なる素晴らしい実装を行う上で i32型にする
-        // match式については少し下の説明を参照しろ
-        let guess:i32 = match guess.trim().parse() {
-            // parseメソッドはエラりやすいので対処してあげる
-            // match式がperseの返す列挙子[Ok || Err]に応じて分枝
-            Ok(num) => num, // num (guessのi32型の奴)を返す
-            Err(_) => { // "_" は何でもいいからマッチしたらという意味。
-                println!("\n貴樣！\n数價を入力(しなさい)");
-                continue; // ここでのcontinue はloopをやり直し。
+        // guess変数(String)と secret_number変数(u32)では型が異っていて比較できない
+        // なので guess変数を u32型にシャドーイングで変換する
+        // trimメソッドは両端の空白や "\n" を全て消してくれる
+        // ユーザが入力を決定して Enter を押した際に改行文字(\n)が文字列に含まれてしまうため trimメソッドで取り除く
+        // perseメソッドは天才的推論で文字列を数値に変換してくれる
+        let guess: i32 = if let Ok(n) = guess.trim().parse() {
+            // 0未満か100超過の場合は ユーザーに伝えて continue でループをやり直す
+            if n < 0 || 100 < n {
+                eprintln!("範囲外ゐ値 {} を取得, 1~100まてゐ整数を入力(しなさい)", n);
+                continue;
             }
+            n
+        } else {
+            // エラーが起きた場合は 問題をユーザーに伝えて continueでループをやり直す
+            eprintln!("\n貴樣！\n数價を入力(しなさい)");
+            continue;
         };
-        
-        // new関連関数で Guessインスタンス としてi32を取り込む
-        let guess = Guess::new(guess).get_value();
-        // 1~100の範囲外な場合はパニックを起こす
-        // get_valueメソッドで値を取り出す(取り出さないと Guess型が代入されてしまう)
 
-        // この {} はプレースホルダーという
-        println!("貴樣ゐ予想: {}", guess); // 次のように予想しました: {}
+        println!("貴樣ゐ予想: {}", guess);
 
-        // std::cmp::Ordering (Ordering型)の列挙子(種類?)は Less, Greater, Equal
-        // cmpメソッドで2値を比較できる。
-        // "<比較対象1>.cmp(&<比較対象2>)" で比較して列挙子を返してくれるので match式* で列挙子に応じた処理をさせる
-        // *match式とは複数のアームから出来ていて与えられた値が各アームと合致した場合そのアームが走る(run)
-        // match式の文法は "match <与える値> { <値1> => <処理1>, <値2> => <処理2>, }" のような感じ
-        // 処理部分は "{ <処理1>; <処理2>; }"" のようにして複数にできる
-        // なお、比較対象1に cmoメソッドを呼び出して比較対象2を参照するので比較対象2には & を付けます
-        // 不変で良いので mut は付けない
+        // cmpメソッドは引数に与えられた値と比較して std::cmp::Ordering列挙型の列挙子を返す
+        // Ordering列挙型の列挙子は Less, Greater, Equal の3つがある
+        // match文を使って列挙子に応じた処理を行う
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("矮小過ぎゑてあゑ"), //小さすぎ！
-            Ordering::Greater => println!("過巨大だ！！"), //大きすぎ！
+            Ordering::Less => println!("矮小過ぎアル！"),
+            Ordering::Greater => println!("過巨大だ！！"),
             Ordering::Equal => {
-                println!("貴樣ゐ勝利てず。\n賞金ゐ1000000000000元を贈呈ずゑ！！"); //やったね！
+                println!("貴樣ゐ勝利てず。\n賞金ゐ1000000000000元を贈呈ずゑ！！");
+
+                // クリア後の応答待機
                 loop {
-                    println!("ウィンドウを閉じる場合は y と入力...");
+                    println!("終了する場合は y と入力...");
+
+                    // ユーザーの選択を持たせる変数
                     let mut is_break = String::new();
-                    io::stdin().read_line(&mut is_break).expect("何力問題が発生レだ！");
+
+                    stdin().read_line(&mut is_break).expect("何力問題が発生レだ！");
+
                     if is_break.trim() ==  String::from("y") { 
-                        break // いまいる match から抜け出してその先にある break で死
+                        // いまいる loop {} から抜け出してその先にある break で死
+                        break
                     } else {
-                        continue // ウィンドウ閉じるかを聞くループでコンティニュ
+                        // y 以外の文字列が入力された場合は continue でループをやり直す
+                        continue
                     }
 
                 }
-                break; // プログラムの正式終了
+
+                // ロープを抜ける
+                // この先に処理はないのでここでプログラムを終了する
+                break;
             }
         }
     }
